3869465604d6e70ef50b96ec3bc69c8a473bf969
Bojin Yao <bojinyao@berkeley.edu>
2019-01-16 23:56:23 -0800

    Incorporated closure to simplify part of the code.

--- firstapp/FirstApp.swift
+++ firstapp/FirstApp.swift
@@ -15,17 +15,19 @@ struct Concentration {
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+            
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         set(newValue) {
             for index in cards.indices {
@@ -63,4 +65,8 @@ struct Concentration {
 }
 
 
-
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
+    }
+}
